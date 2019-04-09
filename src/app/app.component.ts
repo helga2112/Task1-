edc608dfaeea6964469edea5e11646d7862e4b59
Olga_Dolinher <olga.dolinher@thomsonreuters.com>
2019-04-09 16:53:53 +0300

    tast 2 code added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'shop';
+export class AppComponent implements AfterViewInit {
+  appTitleText:string = 'Super Shop';
+
+  @ViewChild('appTitle')
+  titleField: ElementRef<HTMLHeadingElement>;
+
+  ngAfterViewInit() {
+    this.titleField.nativeElement.innerHTML = this.appTitleText;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductComponentComponent } from './product-component/product-component.component';
-import { ProductListComponentComponent } from './product-list-component/product-list-component.component';
+import { CartModule } from './cart/cart.module';
+import { CoreModule } from './core/core.module';
+import { OrderModule } from './order/order.module';
+import { ProductModule } from './product/product.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponentComponent,
-    ProductListComponentComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+
+    CartModule,
+    CoreModule,
+    OrderModule,
+    ProductModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
